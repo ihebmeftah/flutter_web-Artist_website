de54ba897027b9762bba9fcf52f5c216d9ea4bbf
iheb meftah <salimbb041@gmail.com>
2022-03-15 14:29:07 +0100

    update

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -2,15 +2,13 @@ import 'package:get/get.dart';
 
 class Controller extends GetxController {
   int selcted = 0;
+  int curr = 0;
   List<bool> navBackcolor = [true, false, false, false];
   navigation(int? index) {
     selcted = index!;
-    update();
-  }
-
-  onclick(int index, int curr) {
     navBackcolor[curr] = false;
-    navBackcolor[index] = true;
+    navBackcolor[selcted] = true;
+    curr = selcted;
     update();
   }
 }
